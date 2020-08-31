07d8f397ec40e6ce3592275ffad96051db989ef8
Henrique Pereira <wafx.henrique@grupofleury.com.br>
2020-08-31 12:48:15 -0300

    inserindo endpoinsts, arquivos da jetbrains no .ignore e informações do projeto no README.MD

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const {v4: uuid} = require('uuid');
 
-// const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,55 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title = '', url = '', techs = []} = request.body
+  const repository = {id: uuid(), title, url, techs, likes: 0}
+  repositories.push(repository)
+
+  return response.json(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {title = '', url = '', techs = []} = request.body
+  const {id} = request.params
+  const repositoryIndex = repositories.findIndex(repo => repo.id === id)
+  if (repositoryIndex < 0) {
+    return response.status(400).json({error: 'Invalid repository'})
+  }
+
+  const repository = {id: uuid(), title, url, techs, likes: 0}
+  repositories[repositoryIndex] = repository
+
+  return response.json(repository)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params
+  const repositoryIndex = repositories.findIndex(repo => repo.id === id)
+  if (repositoryIndex < 0) {
+    return response.status(400).json({error: 'Invalid repository'})
+  }
+
+  repositories.splice(repositoryIndex)
+
+  return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params
+  const repositoryIndex = repositories.findIndex(repo => repo.id === id)
+  if(repositoryIndex < 0 ){
+    return response.status(400).json({error: 'Invalid repository'})
+  }
+  const repository = repositories[repositoryIndex]
+  repositories[repositoryIndex] = {...repository, likes: repository.likes + 1}
+  const likes= {likes: repositories[repositoryIndex].likes}
+
+  return response.json(likes)
 });
 
 module.exports = app;
